f036af3451fa9afd59fe8aff651a7030c4cf8da6
azane <azane@nnu.edu>
2019-08-14 16:34:39 -0400

    adjusts axis style

--- priority_matrix.py
+++ priority_matrix.py
@@ -3,6 +3,7 @@ import numpy as np
 from matplotlib.patches import Ellipse
 from typing import *
 
+np.random.seed(124125012)
 
 def lrs(f: str) -> (float, float):
     if len(f.split("-")) == 2:
@@ -50,16 +51,22 @@ for f in f2:
     f3.append([name, xw, yh])
 
 fig, ax = plt.subplots(subplot_kw={'aspect': 'equal'})
+ax.plot((0.0, 1.), (1., 0.), ls="-.", c=".7")
+ax.plot((0.0, 1.0), (0.5, 0.5), ls="-", c=".7")
+ax.plot((0.5, 0.5), (0.0, 1.0), ls="-", c=".7")
 
 ells = []
 for f in f3:  # type: Tuple[str, Tuple[float, float], Tuple[float, float]]
     w = (f[1][1]/xscale)*2. if f[1][1] != 1 else .1
     h = (f[2][1]/yscale)*2. if f[2][1] != 1 else .1
+    x = 1. - f[1][0]/xscale
+    y = f[2][0]/yscale
+
     ells.append(Ellipse(
-        xy=(1. - f[1][0]/xscale, f[2][0]/yscale),
+        xy=(x, y),
         width=w,
         height=h,
-        facecolor=(.0, .5, .5, .5),
+        facecolor=(*np.random.rand(3)*.8, .4),
     ))
 
     plt.text(ells[-1].center[0] - w/2., ells[-1].center[1], f[0])
@@ -72,6 +79,8 @@ ax.set_ylabel('Benefit')
 ax.set_xlabel('Ease of Development (Inverse Cost)')
 ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
+ax.spines['left'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
 plt.xticks([])
 plt.yticks([])
 
